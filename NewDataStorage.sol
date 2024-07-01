2d834871570aa876926b93857b17db726fd3af1e
Sashini Dawpadi <86653321+SashiniDawpadi@users.noreply.github.com>
2024-07-01 09:25:22 +0530

    Add Data func updated

--- NewDataStorage.sol
+++ NewDataStorage.sol
@@ -9,10 +9,20 @@ contract NewDataStorage {
     event DataAdded(string data);
 
     // Function to add data to the array
-    function addData(string memory newData) public {
-        dataArray.push(newData);
-        emit DataAdded(newData);
+    function addData(string[] memory newData) public {
+        for (uint i = 0; i < newData.length ; i++) {
+            dataArray.push(newData[i]);
+            emit DataAdded(newData[i]);
+        }
+        // dataArray.push(newData);
+        // emit DataAdded(newData);
     }
+
+    // function addData(string[] memory _data) public {
+    //     for (uint i = 0; i < _data.length; i++) {
+    //         dataStorage.push(_data[i]);
+    //     }
+    // }
 
     // Function to retrieve data from the array by index
     function getData(uint index) public view returns (string memory) {
