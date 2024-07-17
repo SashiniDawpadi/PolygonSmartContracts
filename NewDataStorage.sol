c6e6230e781933d7cd02c6bdbb18ade1edd69abd
Sashini Dawpadi <86653321+SashiniDawpadi@users.noreply.github.com>
2024-07-17 10:39:42 +0530

    getByTxnHash added

--- NewDataStorage.sol
+++ NewDataStorage.sol
@@ -4,30 +4,34 @@ pragma solidity ^0.8.0;
 contract NewDataStorage {
     // Array to store the data
     string[] private dataArray;
+    
+    // Mapping to store data by transaction hash
+    mapping(bytes32 => string) private dataByTxHash;
 
     // Event to be emitted when new data is added
-    event DataAdded(string data);
+    event DataAdded(string data, bytes32 txHash);
 
-    // Function to add data to the array
+    // Function to add data to the array and map it by transaction hash
     function addData(string[] memory newData) public {
-        for (uint i = 0; i < newData.length ; i++) {
+        for (uint256 i = 0; i < newData.length; i++) {
             dataArray.push(newData[i]);
-            emit DataAdded(newData[i]);
+            bytes32 txHash = keccak256(abi.encodePacked(msg.sender, block.timestamp, newData[i]));
+            dataByTxHash[txHash] = newData[i];
+            emit DataAdded(newData[i], txHash);
         }
-        // dataArray.push(newData);
-        // emit DataAdded(newData);
     }
 
-    // function addData(string[] memory _data) public {
-    //     for (uint i = 0; i < _data.length; i++) {
-    //         dataStorage.push(_data[i]);
-    //     }
-    // }
-
     // Function to retrieve data from the array by index
-    function getData(uint index) public view returns (string memory) {
+    function getData(uint256 index) public view returns (string memory) {
         require(index < dataArray.length, "Index out of bounds");
         return dataArray[index];
+    }
+
+    // Function to retrieve data by transaction hash
+    function getDataByTxHash(bytes32 txHash) public view returns (string memory) {
+        string memory data = dataByTxHash[txHash];
+        require(bytes(data).length > 0, "Data not found for the given transaction hash.");
+        return data;
     }
 
     // Function to retrieve all data from the array
@@ -35,3 +39,5 @@ contract NewDataStorage {
         return dataArray;
     }
 }
+
+
